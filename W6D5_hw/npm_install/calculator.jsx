92090ba85abcbf7ac2da8d1d3f319dcda2346fff
Rose <rkoron007@gmail.com>
2018-03-22 22:40:33 -0700

    Started React calculator

---
+++ W6D5_hw/npm_install/calculator.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { numbers: [], operators: [] };
+  }
+
+  render() {
+    return <div />;
+  }
+}
+
+export default Calculator;
