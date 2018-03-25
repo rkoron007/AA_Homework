c3083145124fa94629b67334f72a071819e8c74c
Rose <rkoron007@gmail.com>
2018-03-25 11:43:56 -0700

    Created Dollar Store Widget

---
+++ W7D1_hw/dollar_store/frontend/actions.js
@@ -0,0 +1,11 @@
+const SWITCH_CURRENCY = "SWITCH_CURRENCY";
+
+const selectCurrency = (baseCurrency, rates) => {
+  return {
+    type: "SWITCH_CURRENCY",
+    baseCurrency: baseCurrency,
+    rates: rates
+  };
+};
+
+export default selectCurrency;
