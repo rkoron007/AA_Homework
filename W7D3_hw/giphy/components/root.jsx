71c0dc3a01e39957390d5bd89212034ba23c9cd0
Rose <rkoron007@gmail.com>
2018-03-26 22:42:20 -0700

    Started Giphys

---
+++ W7D3_hw/giphy/components/root.jsx
@@ -0,0 +1,3 @@
+import React from "react";
+import { Provider } from "react-redux";
+import { fetchSearchGiphys } from "../util/api_util";
---
+++ W7D3_hw/giphy/giphy_search.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import configureStore from "./store/store";
+import Root from "./components/root";
+import FetchSearchGiphys from "./util/api_util";
+
+console.log(FetchSearchGiphys);
